fce85fa3b766291285e1faa9169946bdedbe571a
Fahad Guled <fahad.guled@yahoo.com>
2022-02-13 22:02:25 -0800

    made some minor changes and added notes

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 const userInput = document.getElementById('userinput');
 const button = document.getElementById('btn');
 const list = document.getElementById('todo-list');
+const demo = document.getElementById('demo');
 
 
 let listItems = [];
 
 button.addEventListener('click', () => {
-
+    //creating li element everytime button is clicked
     const todo = document.createElement('li');
-
+    //appending li element to html
     list.append(todo);
-
+    //user value everytime they input a todo
     const userText = userInput.value;
-
+    //change the li text to equal user input
     todo.innerHTML = userText;
+    //push todo text to array
+    listItems.push(todo.innerHTML);
+    //setting array list to local storage
+    localStorage.setItem('todo', JSON.stringify(listItems));
+    //clear input box everytime a value is entered //
+    userInput.value = '';
+    //Grabbing items from local storage
+    localStorage.getItem('todo');  
+
+    if(localStorage.getItem('todo')) {
+        JSON.parse(localStorage.getItem('todo'));
+    }
+
+   
+
 
     // list items STYLING //
     todo.style.listStyleType = "number";
@@ -25,20 +41,25 @@ button.addEventListener('click', () => {
     todo.style.fontWeight = "bold";
     todo.style.textTransform = "capitalize";
 
-    listItems.push(todo.innerHTML);
-
-    localStorage.setItem(listItems.indexOf(todo.innerHTML), JSON.stringify(todo.innerHTML) );
+   
+    
+    
 
     
-    //clear input box everytime a value is entered //
-    userInput.value = '';
+    
+    
 
-    console.log(listItems)
+
     
    
     // remove li element on click //
     todo.addEventListener('click', () => {
-        localStorage.removeItem(listItems.indexOf(todo.innerHTML)); //remove from storage
+        for(let i=0; i < listItems.length; i++) {
+            localStorage.removeItem(listItems[i])
+        }
+
+
+
         todo.remove(); // remove from the UI
     })
     
