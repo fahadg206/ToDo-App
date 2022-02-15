62766d4c2abb5d798cbca96c30d8d4ec6f487de3
Fahad Guled <fahad.guled@yahoo.com>
2022-02-15 08:33:16 -0800

    made some changes. Checked if local storage exists outside of button click and display todo items if they did exist. Added more notes to my code.

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const demo = document.getElementById('demo');
 let listItems = [];
 
 button.addEventListener('click', () => {
-    //creating li element everytime button is clicked
+    //creating listTodo element everytime button is clicked
     const todo = document.createElement('li');
-    //appending li element to html
+    //appending listTodo element to html
     list.append(todo);
     //user value everytime they input a todo
     const userText = userInput.value;
-    //change the li text to equal user input
+    //change the listTodo text to equal user input
     todo.innerHTML = userText;
     //push todo text to array
     listItems.push(todo.innerHTML);
@@ -23,14 +23,7 @@ button.addEventListener('click', () => {
     userInput.value = '';
     //Grabbing items from local storage
     localStorage.getItem('todo');  
-
-    if(localStorage.getItem('todo')) {
-        JSON.parse(localStorage.getItem('todo'));
-    }
-
-   
-
-
+    
     // list items STYLING //
     todo.style.listStyleType = "number";
     todo.style.cursor = "pointer";
@@ -41,27 +34,37 @@ button.addEventListener('click', () => {
     todo.style.fontWeight = "bold";
     todo.style.textTransform = "capitalize";
 
-   
-    
-    
-
-    
-    
-    
-
-
-    
-   
-    // remove li element on click //
-    todo.addEventListener('click', () => {
-        for(let i=0; i < listItems.length; i++) {
-            localStorage.removeItem(listItems[i])
-        }
-
-
-
-        todo.remove(); // remove from the UI
-    })
-    
-    
 });
+
+//Check to see if local storage exists. If it does then post on UI//
+    if(localStorage.getItem('todo')) {
+        
+        let listArray = JSON.parse(localStorage.getItem('todo'));
+        
+
+        listArray.forEach((element) => {
+            let listTodo = document.createElement("li");
+            listTodo.innerHTML = element;
+            list.appendChild(listTodo);
+            listTodo.style.listStyleType = "number";
+            listTodo.style.cursor = "pointer";
+            listTodo.style.color = "aliceblue";
+            listTodo.style.fontSize = "22px";
+            listTodo.style.textAlign = "center";
+            listTodo.style.padding = "7px";
+            listTodo.style.fontWeight = "bold";
+            listTodo.style.textTransform = "capitalize";
+
+            listTodo.addEventListener('click', () => {
+                // Remove from UI
+                listTodo.remove();
+                //Remove from local storage
+                localStorage.removeItem(element);
+                
+            })
+        }) 
+
+        
+    }
+
+       
